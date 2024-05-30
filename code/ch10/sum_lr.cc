51a38a89aba47e196d258c2587f5d81aa3e9f072
wyrm <424232810@qq.com>
2024-05-30 17:17:42 +0800

    docs(slide): change course ch09 and ch10

---
+++ code/ch10/sum_lr.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int a[105];
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> a[i];
+  int l, r;  // 表示从 a[l] 累加至 a[r]
+  cin >> l >> r;
+  int sum = 0;
+  for (int i = l; i <= r; i++) sum += a[i];
+  cout << sum << endl;
+
+  return 0;
+}
