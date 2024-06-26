0ee8546f6d24ed3393bbe9b316d543e5e2e6e462
wyrm <81356314+sea-wyrm@users.noreply.github.com>
2024-06-26 15:25:34 +0800

    docs(slide): start ch14

---
+++ code/ch14/isPrime_2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    // i * i <= n 相当于 i <= sqrt(n)
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    if (isPrime(n)) cout << "yes" << endl;
+    else cout << "no" << endl;
+
+    return 0;
+}
