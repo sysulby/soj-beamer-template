9714d232692ab24e4f53c99ea4a5b84eac39aace
wyrm <81356314+sea-wyrm@users.noreply.github.com>
2024-07-11 17:47:23 +0800

    docs(slide): finish ch16

---
+++ code/ch16/find_chopsticks.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+    int a;
+    cin >> a;
+    ans ^= a;
+  }
+  cout << ans << endl;
+  return 0;
+}
